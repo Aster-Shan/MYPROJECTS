b04ca242903d7bb451f8541b40b723f6d2cdecb0
Aster-Shan <thuushanphyoe098@gmail.com>
2025-07-16 04:37:38 +0100

    Pagination

--- Furniture/src/components/products/Pagination.tsx
+++ Furniture/src/components/products/Pagination.tsx
@@ -1,5 +1,38 @@
-function Pagination() {
-  return <div>Pagination</div>;
-}
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
-export default Pagination;
+export default function PaginationBottom() {
+  return (
+    <Pagination className="mb-8">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
