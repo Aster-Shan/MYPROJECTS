242cbc5d6520cd5c51f2e21f81446a04d15da76e
Aster-Shan <thuushanphyoe098@gmail.com>
2025-12-30 01:46:30 +0000

    bug fix for image upload function

---
+++ furniture-backend/src/controllers/api/profileController.ts
@@ -0,0 +1,98 @@
+import { NextFunction, Request, Response } from 'express';
+import { query, validationResult } from 'express-validator';
+import { unlink } from 'node:fs/promises';
+import path from 'path';
+
+import { errorCode } from '../../../config/errorCode';
+import { getUserById, updateUser } from '../../services/authService';
+import { checkUerIfNotExit } from '../../utils/auth';
+import { authorise } from '../../utils/authorise';
+import { checkFileExit } from '../../utils/check';
+
+interface CustomRequest extends Request {
+  userId?: number;
+}
+
+export const changeLanguage = [
+  query('lng', 'Invalid Language code.')
+    .trim()
+    .notEmpty()
+    .matches('^[a-z]+$')
+    .isLength({ min: 2, max: 3 }),
+  (req: CustomRequest, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    // If validation error occurs
+    if (errors.length > 0) {
+      const error: any = new Error(errors[0].msg);
+      error.status = 400;
+      error.code = errorCode.invalid;
+      return next(error);
+    }
+
+    const { lng } = req.query;
+    res.cookie('i18next', lng);
+    res.status(200).json({ message: req.t('changeLan', { lang: lng }) });
+  },
+];
+
+export const testPermission = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const userId = req.userId;
+  const user = await getUserById(userId!);
+  checkUerIfNotExit(user);
+
+  const info: any = {
+    title: 'Testing Permission',
+  };
+  // if user.role === "AUTHOR"
+  // content = "You are an author."
+  const can = authorise(true, user!.role, 'AUTHOR');
+  if (can) {
+    info.content = 'You have permission to read this line';
+  }
+
+  res.status(200).json({ info });
+};
+
+export const uploadProfile = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const userId = req.userId;
+  const image = req.file;
+  const user = await getUserById(userId!);
+  checkUerIfNotExit(user);
+  checkFileExit(image);
+
+  // console.log("Image -----", image);
+  const fileName = image!.filename;
+  // const filePath = image!.path;
+  // const filePath = image!.path.replace("\\", "/");
+
+  if (user?.image) {
+    try {
+      const filePath = path.join(
+        __dirname,
+        '../../..',
+        '/uploads/images',
+        user!.image!,
+      );
+      await unlink(filePath);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const userData = {
+    image: fileName,
+  };
+  await updateUser(user?.id!, userData);
+
+  res.status(200).json({
+    message: 'Profile picture uploaded successfully.',
+    image: fileName,
+  });
+};
