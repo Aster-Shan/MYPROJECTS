e45407b39385f2124ff79f03e1cabcf79dd50062
Aster-Shan <thuushanphyoe098@gmail.com>
2026-04-08 02:44:51 +0100

    add interceptions

--- Furniture/src/api/index.ts
+++ Furniture/src/api/index.ts
@@ -7,4 +7,13 @@ const api = axios.create({
   },
   withCredentials: true,
 });
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = "/lgoin";
+    }
+    return Promise.reject(error);
+  }
+);
 export default api;
