33fb75daee96fe2a7182640493c819753289a88b
Aster-Shan <thuushanphyoe098@gmail.com>
2026-04-13 03:58:49 +0100

    Update Otp.tsx

--- Furniture/src/pages/auth/Otp.tsx
+++ Furniture/src/pages/auth/Otp.tsx
@@ -1,10 +1,10 @@
-import InputOTPForm from "@/components/auth/OtpForm";
+import { OtpForm } from "@/components/auth/OtpForm";
 
 export default function OtpPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <InputOTPForm />
+        <OtpForm />
       </div>
     </div>
   );
