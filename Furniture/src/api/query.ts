60e40be43c8fe70d4d4917bc3adf5faf709a9f0b
Aster-Shan <thuushanphyoe098@gmail.com>
2026-04-16 21:46:26 +0100

    Tanstack query configuration

---
+++ Furniture/src/api/query.ts
@@ -0,0 +1,23 @@
+import { QueryClient } from "@tanstack/react-query";
+import api from ".";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, //5min cache
+    },
+  },
+});
+const fetchproducts = (q?: string) =>
+  api.get(`users/products${q ?? ""}`).then((res) => res.data);
+export const productQuery = (q?: string) => ({
+  queryKey: ["products", q],
+  queryFn: fetchproducts,
+});
+
+const fetchPosts = (q?: string) =>
+  api.get(`users/posts/infinite${q ?? ""}`).then((res) => res.data);
+export const postQuery = (q?: string) => ({
+  queryKey: ["posts", q],
+  queryFn: fetchPosts,
+});
--- Furniture/src/main.tsx
+++ Furniture/src/main.tsx
@@ -1,4 +1,6 @@
+import { queryClient } from "@/api/query";
 import { ThemeProvider } from "@/components/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -7,8 +9,10 @@ import { router } from "./routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router}></RouterProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router}></RouterProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
