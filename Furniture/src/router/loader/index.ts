68f6e901f64c4541c0d1216be43c736934059a2c
Aster-Shan <thuushanphyoe098@gmail.com>
2026-04-17 23:40:18 +0100

    fix cache after fetch

--- Furniture/src/router/loader/index.ts
+++ Furniture/src/router/loader/index.ts
@@ -1,18 +1,19 @@
-import api, { authApi } from "@/api";
+import { authApi } from "@/api";
+import { postQuery, productQuery, queryClient } from "@/api/query";
 import useAuthStore, { Status } from "@/store/authStore";
 import { redirect } from "react-router";
 
-export const homeLoader = async () => {
-  try {
-    const products = await api.get("users/products?limit=8");
-    const posts = await api.get("users/posts/infinite?limit=3");
+// export const homeLoader = async () => {
+//   try {
+//     const products = await api.get("users/products?limit=8");
+//     const posts = await api.get("users/posts/infinite?limit=3");
 
-    return { productsData: products.data, postsData: posts.data };
-  } catch (error) {
-    console.log("HomeLoader eror:", error);
-    throw error;
-  }
-};
+//     return { productsData: products.data, postsData: posts.data };
+//   } catch (error) {
+//     console.log("HomeLoader eror:", error);
+//     throw error;
+//   }
+// };
 export const loginLoader = async () => {
   try {
     const response = await authApi.get("auth-check");
@@ -38,5 +39,10 @@ export const confirmLoader = async () => {
   if (authStore.status !== Status.confirm) {
     return redirect("/register");
   }
+  return null;
+};
+export const homeLoader = async () => {
+  await queryClient.ensureQueryData(productQuery("?limit=8"));
+  await queryClient.ensureQueryData(postQuery("?limit=3"));
   return null;
 };
