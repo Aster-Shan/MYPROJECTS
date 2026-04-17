1bc340a8e9ac4e6e06e39854de44c085e0cd87bb
Aster-Shan <thuushanphyoe098@gmail.com>
2026-04-17 23:39:12 +0100

    Update Header.tsx

--- Furniture/src/components/layouts/Header.tsx
+++ Furniture/src/components/layouts/Header.tsx
@@ -2,6 +2,7 @@ import { siteConfig } from "@/config/site";
 // import { cartItems } from "@/data/carts";
 import { User } from "@/data/user";
 import { ModeToggle } from "../mode-toggle";
+import ProgressBar from "../progess-bar";
 import AuthDropDown from "./AuthDropDown";
 import CartSheet from "./CartSheet";
 import MainNavigation from "./MainNavigation";
@@ -11,6 +12,7 @@ function Header() {
   return (
     <header className="w-full border-b border-gray-200 fixed top-0 bg-white">
       <nav className="container flex items-center h-18 mx-auto">
+        <ProgressBar></ProgressBar>
         <MainNavigation items={siteConfig.mainNav} />
         <MobileNavigation items={siteConfig.mainNav}></MobileNavigation>
         <div className="flex flex-1 items-center justify-end space-x-4 mr-8 lg:mr-0">
