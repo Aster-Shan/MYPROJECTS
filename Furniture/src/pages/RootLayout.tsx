4bcfe671cb874154c271a962e24851605dbeb16d
Aster-Shan <thuushanphyoe098@gmail.com>
2026-04-17 23:39:46 +0100

    update animation

--- Furniture/src/pages/RootLayout.tsx
+++ Furniture/src/pages/RootLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 function RootLayout() {
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen ml-12">
         <Header></Header>
 
         <main className="flex-1 mt-18">
