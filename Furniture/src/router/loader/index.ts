efe1b8dd69ddf05da8816292b179f5cbc264c28b
Aster-Shan <thuushanphyoe098@gmail.com>
2026-04-28 23:31:17 +0100

    add post loader

--- Furniture/src/router/loader/index.ts
+++ Furniture/src/router/loader/index.ts
@@ -1,11 +1,13 @@
 import { authApi } from "@/api";
 import {
+  OnePostQuery,
   postInfiniteQuery,
   postQuery,
   productQuery,
   queryClient,
 } from "@/api/query";
 import useAuthStore, { Status } from "@/store/authStore";
+import type { LoaderFunctionArgs } from "react-router";
 import { redirect } from "react-router";
 
 // export const homeLoader = async () => {
@@ -54,4 +56,13 @@ export const homeLoader = async () => {
 export const blogInfiniteLoader = async () => {
   await queryClient.ensureInfiniteQueryData(postInfiniteQuery());
   return null;
+};
+
+export const postLoader = async ({ params }: LoaderFunctionArgs) => {
+  if (!params.postId) {
+    throw new Error("No Post ID provided");
+  }
+  await queryClient.ensureQueryData(postQuery("?limit=6"));
+  await queryClient.ensureQueryData(OnePostQuery(Number(params.postId)));
+  return { postId: params.postId };
 };
